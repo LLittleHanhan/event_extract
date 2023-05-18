ad6147282ffec8da117f8071b30157b5c30cd8ab
Little Hanhan <1627582619@qq.com>
2023-05-18 21:04:18 +0800

    update

--- predict.py
+++ predict.py
@@ -5,7 +5,16 @@ from transformers import AutoTokenizer, AutoConfig
 from model import myBert
 from var import device, id2label
 
-sentence = '它死了'
+label_path = './DuEE1.0/label.txt'
+label2id = {}
+id2label = {}
+with open(label_path, 'r', encoding='utf-8') as f:
+    for line in f.readlines():
+        id, label = line.strip().split(' ')
+        label2id[label] = int(id)
+        id2label[int(id)] = label
+
+sentence = '如何看待“恒大国脚因伤退出国足后迅速在联赛复出”这个热议话题'
 
 tokenizer = AutoTokenizer.from_pretrained('./chinese-roberta-wwm-ext')
 config = AutoConfig.from_pretrained('./chinese-roberta-wwm-ext')
@@ -15,8 +24,24 @@ model.eval()
 with torch.no_grad():
     inputs = tokenizer(sentence, truncation=True, return_tensors="pt", return_offsets_mapping=True, max_length=512)
     offsets = inputs.pop('offset_mapping').squeeze(0)
-    _, pred = model(inputs)
+    print(offsets[0])
+    start,end = offsets[0]
+    print(start,end)
+    pred = model(inputs)[1][0][1:-1]
     print(pred)
-    for idx,token in enumerate(pred):
-
-
+    idx = 0
+    trigger = 0
+    trigger_start = trigger_end = []
+    while idx < len(pred):
+        if pred[idx] % 2 == 1:
+            print('yes')
+            trigger = pred[idx]
+            start, end = offsets[idx]
+            trigger_start.append(start)
+        while idx+1 < len(pred) and pred[idx + 1] == trigger + 1:
+            idx += 1
+        _, end = offsets[idx]
+        trigger_end.append(end)
+        idx += 1
+    print(trigger_start)
+    print(trigger_end)
